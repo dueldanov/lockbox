1864a9ea70f1ed7fb32bceec85edd9f488536b31
Dinis Ueldanov <38666769+dueldanov@users.noreply.github.com>
2025-07-10 13:27:47 +0500

    Update main.go via AI assistant

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"github.com/iotaledger/hornet/v2/components/app"
+    "github.com/iotaledger/lockbox/v2/components/app"
 )
 
 func main() {
-	app.App().Run()
+    app.App().Run()
 }
